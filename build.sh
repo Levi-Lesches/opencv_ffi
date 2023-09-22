49a44e981d5a5f7ada1107aab8508a7683d53945
Binghamton University Rover Team <rover@binghamton.edu>
2023-09-21 21:52:18 -0400

    Cleaned up Linux script

--- build.sh
+++ build.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-# Find CMake and warn if it's not installed
+# Warn if CMake is not installed
 CMAKE_PATH=`which cmake`
 if [ -z "$CMAKE_PATH" ]; then
 	echo CMake could not be found. Please install it by running
 	echo sudo apt install cmake
 	exit 1
+fi
+
+# Warn if cannot find OpenCV
+OPENCV_DIR="./src/opencv"
+if [ ! -d $OPENCV_DIR ]; then
+	echo Could not find OpenCV.
+	echo Make sure you are running this in the opencv_ffi package.
+	exit 2
 fi
 
 # Build OpenCV and opencv_ffi
@@ -17,8 +25,17 @@ cd ..
 
 # Copy the .so files to the .dist directory
 mkdir -p dist
-cp XXX/*.so dist  # the OpenCV libraries
-cp XXX/*.so dist  # the opencv_ffi library
+cp build/opencv/lib/*.so dist  # the OpenCV libraries
+cp build/*.so dist  # the opencv_ffi library
 
+cd $(dirname "$0")
+SCRIPT_DIR=$(pwd)
+INDENT="  "
+
+echo 
 echo Done! Your files are in the dist folder
-echo Copy these to the root of whatever project you use opencv_ffi in
+echo To let your device find them, run this command:
+echo "  echo \"export LD_LIBRARY_PATH=\\\$LD_LIBRARY_PATH:$SCRIPT_DIR/dist\" >> ~/.bashrc"
+echo You only need to run this once, but run this again if you move this folder
+echo This command does not affect any open terminal shells or SSH sessions.
+echo You\'ll need to open a new shell or SSH again for it to take effect.
