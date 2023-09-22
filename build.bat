6ccb5be8bca646a9754c0a9e0277d81653c270c8
Levi Lesches <levilesches@gmail.com>
2023-09-22 14:42:33 -0400

    Added error check for missing OpenCV on Windows

--- build.bat
+++ build.bat
@@ -5,8 +5,14 @@ where cmake 1>NUL 2>NUL
 if errorlevel 1 (
 	echo You must run this script with cmake on your PATH
 	echo The easiest way to do this is to install Visual Studio, which you did if you're using Flutter for Windows,
-	echo and run this script from the Developer Command Prompt for Visual Studio. Search that in your Start menu
+	echo and run this script from the Developer Command Prompt for Visual Studio. Search that in your Start menu.
 	exit /b 1
+)
+
+git submodule update --init --recursive
+if errorlevel 1 (
+	echo Could not clone OpenCV. Check your internet connection and try again.
+	echo You can also do this manually with ^"git submodule update --init --recursive^"
 )
 
 rem Build OpenCV and opencv_ffi
