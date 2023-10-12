79c36e804f72bb3bdbfe7d4d2d90b12564e3bf15
pgrwe <aldreichampadu@outlook.com>
2023-10-12 00:38:21 -0400

    working on pull request

--- example/example.dart
+++ example/example.dart
@@ -2,14 +2,13 @@ import "dart:io";
 import "package:opencv_ffi/opencv_ffi.dart";
 
 void main() async {
-  int myint = 0;
   final camera =
       Platform.isWindows ? Camera.fromIndex(0) : Camera.fromName("/dev/video0");
   camera.zoom(0);
   try {
-    runtime(camera);
+    runtime(Camera camera);
   } on CameraReadException {
-    runtime(camera);
+    runtime(Camera camera);
   } finally {
     camera.dispose();
   }
