248072013bb5f657794cff2d31962952b136cc05
pgrwe <aldreichampadu@outlook.com>
2023-10-12 01:05:43 -0400

    working on pull request pt 3

--- example/example.dart
+++ example/example.dart
@@ -6,10 +6,6 @@ void main() async {
   final camera =
       Platform.isWindows ? Camera.fromIndex(0) : Camera.fromName("/dev/video0");
 
-  /// Note: getters will output -1 if the video device does not support a specific setting
-  print(camera.zoom); 
-  print(camera.fps);
-
   for (var i = 0; i < 3; i++){
     try {
       runtime(camera);
@@ -17,13 +13,13 @@ void main() async {
 
       switch(i){
         case 0: 
-          print('Check your device name');
+          print("Check your device name");
         case 1:
         case 2:
-          print('Camera disconnected, restarting');
+          print("Camera disconnected, restarting");
           runtime(camera);
         case 3:
-          print('Camera disconnected 3 times, exiting');
+          print("Camera disconnected 3 times, exiting");
       }
 
     } finally {
