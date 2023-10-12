6c7ae95f5fa9b2db7bd6a1043c2aba615b84c99c
pgrwe <aldreichampadu@outlook.com>
2023-10-12 01:03:41 -0400

    working on pull request pt 2

--- example/example.dart
+++ example/example.dart
@@ -1,21 +1,40 @@
 import "dart:io";
 import "package:opencv_ffi/opencv_ffi.dart";
 
+
 void main() async {
   final camera =
       Platform.isWindows ? Camera.fromIndex(0) : Camera.fromName("/dev/video0");
-  camera.zoom(0);
-  try {
-    runtime(Camera camera);
-  } on CameraReadException {
-    runtime(Camera camera);
-  } finally {
-    camera.dispose();
+
+  /// Note: getters will output -1 if the video device does not support a specific setting
+  print(camera.zoom); 
+  print(camera.fps);
+
+  for (var i = 0; i < 3; i++){
+    try {
+      runtime(camera);
+    } on CameraReadException {
+
+      switch(i){
+        case 0: 
+          print('Check your device name');
+        case 1:
+        case 2:
+          print('Camera disconnected, restarting');
+          runtime(camera);
+        case 3:
+          print('Camera disconnected 3 times, exiting');
+      }
+
+    } finally {
+      camera.dispose();
+    }
   }
 }
 
-void runtime(camera){
+void runtime(Camera camera){
   while (true) {
     camera.showFrame();
   }
 }
+
